18e60fef02c22c819b82dc13c089199441bb1470
Fajar <fajarmh2@gmail.com>
2022-05-21 19:48:30 +0700

    Adding article show /:id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
-# supaya bisa komunikasi dengan table dan ngasih getter dan setter
+# mode kosong ini berfungsi untuk
+# komunikasi dengan table dan ngasih getter dan setter
+ 
+  # Validations; Presence = No NIL
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles, only: [:show] # Kalau pengen 1 aja
 end
