d85b7244e00d9f36df43f104ddb4770816ea3679
Fajar <fajarmh2@gmail.com>
2022-05-22 10:13:24 +0700

    Refactoring code in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -29,7 +29,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description, :theme))
       flash[:notice] = "Article was updated succesfully"
       redirect_to @article
@@ -39,8 +38,14 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
