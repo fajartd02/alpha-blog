89a49df3d29972cd63887fc21d3c47cd860a6613
Fajar <fajarmh2@gmail.com>
2022-05-24 22:50:23 +0700

    Use before save in model for emaill downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
@@ -7,5 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, 
                     uniqueness: { case_sensitive: false }, 
                     length: { maximum: 105 },
-                    format: { with: VALID_EMAIL_REGEX }        
+                    format: { with: VALID_EMAIL_REGEX }
+  
 end
