9ad881c80404e78950e07d3255847e91d9ed5047
Fajar <fajarmh2@gmail.com>
2022-05-24 23:19:17 +0700

    Adding sign up form views

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # Kalau pengen specific 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
