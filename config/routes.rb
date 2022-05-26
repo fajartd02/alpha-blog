4386fbcd32b76b8abebfd2e5d0cff0321a537228
Fajar <fajarmh2@gmail.com>
2022-05-26 23:31:51 +0700

    Create sessios

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  # Login
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'sessions#destroy'
 end
